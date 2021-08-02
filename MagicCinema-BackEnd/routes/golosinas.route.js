6d9a52aee0cbddbe1bc357075e53dd700b271e06
Diego Mercadal <dmecadal-2016490@kinal.edu.gt>
2021-08-01 19:16:13 -0600

    Correcion de rutas para Golosinas

--- MagicCinema-BackEnd/routes/golosinas.route.js
+++ MagicCinema-BackEnd/routes/golosinas.route.js
@@ -8,12 +8,12 @@ const upload = connectMultiparty({uploadDir: './uploads/golosina'});
 
 var api = express.Router();
 
-api.put('/:idU/addGolosina/:idC', [mdAuth.ensureAuth,mdAuth.ensureAuthAdmin], golosinasController.addGolosina);
-api.put('/:idU/updateGolosina/:idC/:idG', [mdAuth.ensureAuth,mdAuth.ensureAuthAdmin], golosinasController.updateGolosina);
-api.put('/:idU/deleteGolosina/:idC/:idG', [mdAuth.ensureAuth,mdAuth.ensureAuthAdmin], golosinasController.deleteGolosina);
+api.put('/:idU/addGolosina/:idC', [mdAuth.ensureAuth,mdAuth.ensureAuthAdminCine], golosinasController.addGolosina);
+api.put('/:idU/updateGolosina/:idC/:idG', [mdAuth.ensureAuth,mdAuth.ensureAuthAdminCine], golosinasController.updateGolosina);
+api.put('/:idU/deleteGolosina/:idC/:idG', [mdAuth.ensureAuth,mdAuth.ensureAuthAdminCine], golosinasController.deleteGolosina);
 api.get('/getGolosinas/:idC', golosinasController.getGolosinas);
 
-api.put('/:idU/uploadImageGolosina/:idG', [mdAuth.ensureAuth,mdAuth.ensureAuthAdmin,upload], golosinasController.uploadImageGolosina);
+api.put('/:idU/uploadImageGolosina/:idG', [mdAuth.ensureAuth,mdAuth.ensureAuthAdminCine,upload], golosinasController.uploadImageGolosina);
 api.get('/getImageGolosina/:fileName', [upload], golosinasController.getImageGolosina);
 
 
